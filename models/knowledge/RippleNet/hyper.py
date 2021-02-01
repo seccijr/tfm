e5dc593bac1439b6d49d251c78e5b44c03afee23
Carlos Pérez Sechi <carlos.perez@di-dy.com>
2021-02-01 09:16:57 +0100

    Going for Knowledge Graphs Only

---
+++ models/knowledge/RippleNet/hyper.py
@@ -0,0 +1,20 @@
+import os
+from datetime import datetime
+from recbole.trainer import HyperTuning
+from recbole.quick_start import objective_function
+
+hp = HyperTuning(
+    objective_function=objective_function, algo='exhaustive',
+    params_file=f'models{os.path.sep}Knowledge{os.path.sep}RippleNet{os.path.sep}hyper.test',
+    fixed_config_file_list=[f'models{os.path.sep}Knowledge{os.path.sep}RippleNet{os.path.sep}config.yaml']
+)
+
+# run
+hp.run()
+# export result to the file
+hp.export_result(output_file=f'models{os.path.sep}results{os.path.sep}RippleNet-{datetime.now():%Y%m%d%H%M%S%z}.result')
+# print best parameters
+print('best params: ', hp.best_params)
+# print best result
+print('best result: ')
+print(hp.params2result[hp.params2str(hp.best_params)])
