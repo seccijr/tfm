b357292a63f654da19179ea1529adc7a76d37780
Carlos Pérez Sechi <carlos.perez@di-dy.com>
2021-02-01 09:20:19 +0100

    Keeping only Models folder hierarchy to simplify structure

--- models/RippleNet/hyper.py
+++ models/RippleNet/hyper.py
@@ -5,14 +5,14 @@ from recbole.quick_start import objective_function
 
 hp = HyperTuning(
     objective_function=objective_function, algo='exhaustive',
-    params_file=f'models{os.path.sep}Knowledge{os.path.sep}MKR{os.path.sep}hyper.test',
-    fixed_config_file_list=[f'models{os.path.sep}Knowledge{os.path.sep}MKR{os.path.sep}config.yaml']
+    params_file=f'models{os.path.sep}RippleNet{os.path.sep}hyper.test',
+    fixed_config_file_list=[f'models{os.path.sep}RippleNet{os.path.sep}config.yaml']
 )
 
 # run
 hp.run()
 # export result to the file
-hp.export_result(output_file=f'models{os.path.sep}results{os.path.sep}MKR-{datetime.now():%Y%m%d%H%M%S%z}.result')
+hp.export_result(output_file=f'results{os.path.sep}RippleNet-{datetime.now():%Y%m%d%H%M%S%z}.result')
 # print best parameters
 print('best params: ', hp.best_params)
 # print best result
